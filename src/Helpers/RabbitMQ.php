817bd4729053da73fdee35252f4b878bdeb71ec9
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-12-04 23:31:00 +0600

    Comment added

--- src/Helpers/RabbitMQ.php
+++ src/Helpers/RabbitMQ.php
@@ -9,11 +9,22 @@ class RabbitMQ
 {
     public $rabbitMqJob;
 
+    /**
+     * Set RabbitMq Job Instance
+     *
+     * @param RabbitMQJob $rabbitMqJob
+     * @return void
+     */
     public function setRabbitMqJob(RabbitMQJob $rabbitMqJob): void
     {
         $this->rabbitMqJob = $rabbitMqJob;
     }
 
+    /**
+     * Get RabbitMq Job Instance
+     *
+     * @return void
+     */
     public function getRabbitMqJob(): RabbitMQJob
     {
         return $this->rabbitMqJob;
